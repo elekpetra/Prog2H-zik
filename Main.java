8e58c725535c3a7c4700a0d935973c791744c251
elekpetra <43540460+elekpetra@users.noreply.github.com>
2018-10-01 23:07:30 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,17 @@
+
+
 public class Main {
+    public static int Osztás(int a, int b){
+        int hanyados=0;
+        do{
+            hanyados=hanyados+1;
+            a=a-b;
+        }while(a>=b);
+        return hanyados;
+    }
     public static void main(String[] args) {
-        Rectangle a=new Rectangle(4,40);
-        Rectangle b=new Rectangle(3.5,35.9);
-        
-        System.out.println("'A' téglalap szélessége: "+a.getWidth()+"\n"+
-            "'A' téglalap magassága: "+a.getHeight()+"\n"+
-            "'A' tégalap területe: "+a.getArea()+"\n"+
-            "'A' téglalap kerülete: "+a.getPrimeter());
-        
-        System.out.println("'B' téglalap szélessége: "+b.getWidth()+"\n"+
-            "'B' téglalap magassága: "+b.getHeight()+"\n"+
-            "'B' tégalap területe: "+b.getArea()+"\n"+
-            "'B' téglalap kerülete: "+b.getPrimeter());
         
         
+        System.out.println(Osztás(122,32));
     }
 }
