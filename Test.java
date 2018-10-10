6dcefdae9a4abb121b16a14e65e6d3185de3fe4b
elekpetra <43540460+elekpetra@users.noreply.github.com>
2018-10-10 21:21:31 +0200

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,31 +1,43 @@
+import java.util.Random;
+
 
 public class Test {
-    public static void main(String[] args) {
-        
-        LinearEquation e1=new LinearEquation(1,2,3,4,5,6);
-        LinearEquation e2=new LinearEquation(5,8,4,9,3,4);
-        LinearEquation e3=new LinearEquation(12,54,62,22,59,4);
-        
-        if(e1.isSolvable()==true){
-        System.out.println("Az első egyenlet eredménye: "+"x1="+e1.getX()+" x2="+e1.getY()+"\n");
-        }
-        else if((e1.getA()*e1.getD()-e1.getB()*e1.getC())==0){
-            System.out.println("The equation has no solution"+"\n");
-        }
-        
-        if(e2.isSolvable()==true){
-        System.out.println("A második egyenlet eredménye: "+"x1="+e2.getX()+" x2="+e2.getY()+"\n");
-        }
-        else if((e2.getA()*e2.getD()-e2.getB()*e2.getC())==0){
-            System.out.println("The equation has no solution"+"\n");
-        }
-        
-        if(e3.isSolvable()==true){
-        System.out.println("A hamadik egyenlet eredménye: "+"x1="+e3.getX()+" x2="+e3.getY()+"\n");
-        }
-        else if((e3.getA()*e3.getD()-e3.getB()*e3.getC())==0){
-            System.out.println("The equation has no solution"+"\n");
-        }
     
+ public static void main(String[] args) {
+ Random rnd = new Random();
+ int[] t = new int[30];
+ for (int i = 0; i < t.length; i++) {
+ t[i] = rnd.nextInt(10);
+ }
+ 
+ printArray(t);
+ long timeStart = System.currentTimeMillis();
+ insertionSort(t);
+ long timeEnd = System.currentTimeMillis();
+ printArray(t);
+ System.out.println(timeEnd - timeStart);
+ }
+ static void printArray(int[] a) {
+ for (int i = 0; i < a.length; i++) {
+ System.out.print(a[i] + " ");
+ }
+ System.out.println();
+ }
+ static void insertionSort(int[] a) {
+ // Write your code here.
+     
+        for (int i=1; i<a.length; ++i) 
+        { 
+            int k = a[i]; 
+            int j = i-1; 
+  
+
+            while (j>=0 && a[j] > k) 
+            { 
+                a[j+1] = a[j]; 
+                j = j-1; 
+            } 
+            a[j+1] = k; 
+ }
 }
 }
