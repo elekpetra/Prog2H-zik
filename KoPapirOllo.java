21de9a946a802fa6d6fa25f42f4adbb2cd33bd90
elekpetra <43540460+elekpetra@users.noreply.github.com>
2018-10-10 23:17:56 +0200

    Update KoPapirOllo.java

--- KoPapirOllo.java
+++ KoPapirOllo.java
@@ -1,16 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Admin
- */
 public class KoPapirOllo {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
