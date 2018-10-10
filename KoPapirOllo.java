50a74bcc1115f5119f217944c3e689a33f0ea871
elekpetra <43540460+elekpetra@users.noreply.github.com>
2018-10-10 23:03:52 +0200

    Add files via upload

---
+++ KoPapirOllo.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class KoPapirOllo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        
+       while(true){
+          
+         String x;
+        x = sc.nextLine();
+        if(x.equals("vege")){
+            break;
+        }
+        if(!x.equals("ko") && !x.equals("papir") && !x.equals("ollo")){
+            continue;
+        }
+        Random random = new Random();
+        int gepValaszt=random.nextInt(3);
+           System.out.println("Gép ezt adta:" );
+        if (gepValaszt==0){
+            System.out.println("Kő");
+         }
+        if (gepValaszt==1){
+            System.out.println("Papír");
+         }
+        if (gepValaszt==2){
+            System.out.println("olló");
+         }
+        
+        if(x.equals("ko") && gepValaszt==0 ){
+            System.out.println("Döntetlen");
+        }
+        else if(x.equals("ko") && gepValaszt==1){
+            System.out.println("Gép nyert");
+        }
+        else if(x.equals("ko") && gepValaszt==2){
+            System.out.println("Ön nyert");
+        }
+       }
+        
+        
+        
+    }
+}
